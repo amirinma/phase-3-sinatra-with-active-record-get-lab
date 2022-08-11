4be2498035279eed18d9289a4cb201675267634f
Mohammad <zia.ro@live.com>
2022-08-11 12:12:19 -0400

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
-
+get '/bakeries' do
+  bakery = Bakery.all 
+  bakery.to_json 
+end
+get '/bakeries/:id' do 
+  bakery = Bakery.find(params[:id])
+  bakery.to_json(include: :baked_goods)
+end
+get '/baked_goods/by_price' do
+  baked_goods = BakedGood.by_price
+  baked_goods.to_json
+end
+get '/baked_goods/most_expensive' do
+  most_expensive = BakedGood.by_price.first
+  most_expensive.to_json 
+end
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,8 @@
+require "pry"
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+  def self.by_price
+    self.all.sort_by{|bg| bg.price}.reverse
+  end
 end
